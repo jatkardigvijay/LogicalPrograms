157ed1603025f42bb8faa0903c05865ad0d40bb9
Digvijay Jatkar <jatkardigvijay@gmail.com>
2026-05-02 23:38:09 +0530

    StringPalindrome with constraints

--- src/com/jbd/string/StringPalindromeWithConstraints.java
+++ src/com/jbd/string/StringPalindromeWithConstraints.java
@@ -1,5 +1,32 @@
 package com.jbd.string;
 
+import java.util.Scanner;
+
 public class StringPalindromeWithConstraints {
 
+	public static void main(String[] args) {
+		
+		String string, string1;
+		StringBuilder string2 = new StringBuilder();
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a string : ");
+		string = input.nextLine();
+		input.close();
+		string = string.trim().toLowerCase().replaceAll(" ", "");
+		for (char c : string.toCharArray()) {
+			if (Character.isLetterOrDigit(c)) {
+				string2.append(Character.toLowerCase(c));
+			}
+		}
+		StringBuilder reversed = new StringBuilder();
+		for (int i = string2.length()-1; i >= 0; i--) {
+			reversed.append(string2.charAt(i));
+		}
+		if (reversed.toString().equals(string2.toString())) {
+			System.out.println("P");
+		} else {
+			System.out.println("NP");
+		}
+		
+	}
 }
